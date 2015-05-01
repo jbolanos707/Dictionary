ee0a7009f5a3da7c19a4a441378faf42197690c4
Jennifer <jbolanos707@gmail.com>
2015-05-01 16:47:17 -0700

    continue to work on integration

--- app.rb
+++ app.rb
@@ -7,23 +7,19 @@ also_reload('lib/**/*.rb')
 
 get('/') do
   @words = Dictionary.all()
-  word = params.fetch('word')
-  new_word = Dictionary.new(word)
-  new_word.save()
   erb(:index)
 end
 
-get('nw') do
-  erb(:nw)
+post('/word') do
+  @word = params.fetch('word').word()
+  dictionary = Dictionary.new(word)
+  dictionary.save()
+  erb(:index)
 end
 
 post('/definition') do
-  definition = params.fetch('definition')
-  new_definition = Definition.new(definition)
-  new_definition.save()
-  erb(:definiton)
-end
-
-get('nd') do
+  @definition = params.fetch('definition')
+  @new_definition = Definition.new(definition)
+  @new_definition.save()
   erb(:nd)
 end
